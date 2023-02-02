6f8ef6224c2efd2271829c7b3b87514f5fe47c2a
Donatella Tesei <donatella.tesei@gmail.com>
2023-02-02 11:45:10 +0100

    exercise 1

---
+++ Donatella/index.js
@@ -0,0 +1,17 @@
+// Basic
+
+//ex1
+let temp = Math.floor(Math.random() * 25) - 5;
+
+if (temp >= -5 && temp <= 10) {
+    console.log("The weather is cold");
+} else {
+    console.log("The weather is moderate");
+}
+
+console.log(temp);
+
+
+//ex2
+
+// let favoriteFoods[];
