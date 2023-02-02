466682f5e74e795e4de698f2b6f9c111ffff4813
king-of-chardonnay <tanervk@gmail.com>
2023-02-02 11:52:24 +0100

    Ex-1 Finished

---
+++ Taner/script.js
@@ -0,0 +1,10 @@
+let temp = Math.floor(Math.random() * 25 - 5);
+if (temp >= -5 && temp <= 10) {
+    console.log("The Weather is Cold");
+} else {
+    console.log("The Weather is Moderate");
+}
+
+console.log(temp);
+
+// Exercise 2
